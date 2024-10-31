68d1616fb69f619a7e786d59de881ac77acc50e8
ParkHeungJun <p2005hihi@gmail.com>
2024-10-31 20:08:35 +0900

    first commit

---
+++ app/src/main/java/com/heungjun/gaincontrol/AuthViewModel.kt
@@ -0,0 +1,75 @@
+package com.heungjun.gaincontrol
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthViewModel : ViewModel() {
+
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> = _authState
+
+    init {
+        checkAuthStatus()
+    }
+
+
+    fun checkAuthStatus(){
+        if(auth.currentUser==null){
+            _authState.value = AuthState.Unauthenticated
+        }else{
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+    fun login(email : String,password : String){
+
+        if(email.isEmpty() || password.isEmpty()){
+            _authState.value = AuthState.Error("Email or password can't be empty")
+            return
+        }
+        _authState.value = AuthState.Loading
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener{task->
+                if (task.isSuccessful){
+                    _authState.value = AuthState.Authenticated
+                }else{
+                    _authState.value = AuthState.Error(task.exception?.message?:"Something went wrong")
+                }
+            }
+    }
+
+    fun signup(email: String, password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            _authState.value = AuthState.Error("이메일과 비밀번호를 모두 입력해야 합니다.")
+            return
+        }
+        _authState.value = AuthState.Loading
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // 회원가입 완료 시 상태를 Unauthenticated로 변경
+                    _authState.value = AuthState.Unauthenticated
+                } else {
+                    _authState.value = AuthState.Error(task.exception?.message ?: "Something went wrong")
+                }
+            }
+    }
+
+    fun signout(){
+        auth.signOut()
+        _authState.value = AuthState.Unauthenticated
+    }
+
+
+}
+
+sealed class AuthState{
+    object Authenticated : AuthState()
+    object Unauthenticated : AuthState()
+    object Loading : AuthState()
+    data class Error(val message : String) : AuthState()
+}
---
+++ app/src/main/java/com/heungjun/gaincontrol/pages/LoginPage.kt
@@ -0,0 +1,104 @@
+package com.heungjun.gaincontrol.pages
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.heungjun.gaincontrol.AuthState
+import com.heungjun.gaincontrol.AuthViewModel
+
+@Composable
+fun LoginPage(modifier: Modifier = Modifier,navController: NavController,authViewModel: AuthViewModel) {
+
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.Authenticated -> navController.navigate("home")
+            is AuthState.Error -> Toast.makeText(context,
+                (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT).show()
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login", fontSize = 32.sp)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "이메일")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "비밀번호")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            authViewModel.login(email,password)
+        },
+            enabled = authState.value != AuthState.Loading
+        ) {
+            Text(text = "로그인")
+        }
+
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextButton(onClick = {
+            navController.navigate("signup")
+        }) {
+            Text(text = "회원가입 하러 가기")
+        }
+
+    }
+
+}
